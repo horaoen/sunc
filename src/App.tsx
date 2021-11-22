558f1245e2c9c2f3a1a26f42df953e409e055a4c
bleso <bleso624@gmail.com>
2021-11-22 20:34:32 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./Hello";
-import LikeButton from "LikeButton";
-import MouseTracker from "MouseTracker";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-              <Hello />
-              <LikeButton />
-              <MouseTracker />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IThemeProps {
+  [key: string]: { color: string; background: string };
 }
+
+const themes: IThemeProps = {
+  light: {
+    color: "#000",
+    background: "#eee",
+  },
+  dark: {
+    color: "#fff",
+    background: "#222",
+  },
+};
+
+export const ThemeContext = React.createContext(themes.light);
+
+const App: React.FC = () => {
+  const [show, setShow] = useState(true);
+  return <div className="App"></div>;
+};
 
 export default App;
